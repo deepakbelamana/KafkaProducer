32d0ec96c59f8e3eb7e5522380d72cf7fc3046db
Deepak <saideepak627@gmail.com>
2026-04-24 10:48:07 +0530

    kafka producer config updated with spring bean configuration

--- src/main/java/com/appsdeveloperblog/ws/products/config/KafkaConfig.java
+++ src/main/java/com/appsdeveloperblog/ws/products/config/KafkaConfig.java
@@ -1,16 +1,74 @@
 package com.appsdeveloperblog.ws.products.config;
 
 
+import com.appsdeveloperblog.ws.products.service.ProductCreatedEvent;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
+    /*
+        configs defined in java has higher priority, it will override the one present
+        in applicaiton properties, so if we want to change the configs,
+         we can change it here in java code or by loading from database, and it will override the one present in application properties
+     */
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.producer.key-serializer}")
+    private String keySerializer;
+
+    @Value("${spring.kafka.producer.value-serializer}")
+    private String valueSerializer;
+
+    @Value("${spring.kafka.producer.acks}")
+    private String acks;
+
+    @Value("${spring.kafka.producer.properties.delivery.timeout.ms}")
+    private String deliveryTimeout;
+
+    @Value("${spring.kafka.producer.properties.linger.ms}")
+    private String linger;
+
+    @Value("${spring.kafka.producer.properties.request.timeout.ms}")
+    private String requestTimeout;
+
+    Map<String, Object> producerConfigs() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
+        config.put(ProducerConfig.ACKS_CONFIG, acks);
+        config.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeout);
+        config.put(ProducerConfig.LINGER_MS_CONFIG, linger);
+        config.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeout);
+
+        return config;
+    }
 
     @Bean
-    NewTopic createProductCreatedEventTopic(){
+    ProducerFactory<String, ProductCreatedEvent> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate() {
+        return new KafkaTemplate<String, ProductCreatedEvent>(producerFactory());
+    }
+
+    @Bean
+    NewTopic createProductCreatedEventTopic() {
         return TopicBuilder.name("product-created-event")
                 .partitions(3)
                 .build();
